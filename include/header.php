4419ae44d8ed9a218ea75e12f4a95b6211a64287
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2022-05-30 10:29:34 +0530

    Transformed to Php and code optimzation V-1.0.2

---
+++ include/header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>CDH - CAREER DEVELOPMENT HUB</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <!-- Favicon -->
+    <link href="/CDH_logo.png" rel="icon" />
+
+    <!-- Google Font -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <!-- CSS Libraries -->
+    <link
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+      rel="stylesheet"
+    />
+    <link href="lib/animate/animate.min.css" rel="stylesheet" />
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+    <link href="css/style.css" rel="stylesheet" />
+    <style>
+      body {
+      }
+      a {
+        text-decoration: black;
+        color: white;
+      }
+
+      @media (max-width: 992px) {
+        .navbar .ml-auto .nav-item {
+          color: var(--secondary-color);
+        }
+      }
+
+      .navbar-light .navbar-nav .nav-link:hover {
+        color: red;
+      }
+
+      /* new desktop navbar */
+
+      header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      header .logo {
+        display: flex;
+        width: 100px;
+        padding: 10px;
+      }
+      nav ul li {
+        display: inline-block;
+        align-content: center;
+      }
+      .club_logo {
+        width: 76px;
+        margin-left: 10px;
+      }
+      nav ul li a {
+        margin-right: 10px;
+        font-size: 18px;
+      }
+    </style>
+  </head>
+
+  <body data-spy="scroll" data-target=".navbar" data-offset="51">
+    <!-- Nav Bar Start -->
+    <header>
+      <div class="logo">
+        <img class="club_logo" src="CDH_logo.png" alt="" srcset="" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="home.html">Home</a></li>
+          <li><a href="#service">Projects</a></li>
+          <li><a href="#experience">Project Ideas</a></li>
+          <li><a href="https://codingwizarditer.github.io/AGDOC/">AGDOC</a></li>
+          <li><a href="courses.php">Courses</a></li>
+          <li><a href="events.html">Events</a></li>
+          <li><a href="">Organisers</a></li>
+          <li><a href="#contact">Contact us</a></li>
+        </ul>
+      </nav>
+    </header>
